06fb3aac6c4020650c38cad814611f63ac6aa4ef
Sean Stephenson <sms@pixar.com>
2017-03-20 17:07:26 -0700

    add counter

---
+++ src/Form.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Prompt } from 'react-router-dom'
+
+
+class Form extends React.Component {
+  state = {dirty: false}
+  setDirty = () => this.setState({dirty: true})
+  render() {
+    return(
+      <div>
+        <h1>Form</h1>
+        <input type="text" onInput={this.setDirty} />
+        <Prompt
+          when={this.state.dirty}
+          message="Data will be lost!"
+        />
+      </div>
+    )
+  }
+}
+
+export default Form;
